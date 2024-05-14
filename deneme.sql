0b8d454c66fb4cc5e4797cf10acffd44ef6d5f6c
KubraOzkul <Kubraozkul0@gmail.com>
2024-05-14 19:20:49 +0300

    Update deneme.sql

--- deneme.sql
+++ deneme.sql
@@ -1,4 +1,19 @@
---create database bilet_satis
---use bilet_satis
---create table yolcu
+create database biletSatis
+use biletSatis
 
+--Bu tabloda iller tutulur.
+create table tblIl(
+ilID char(2) primary key,
+--Türkiyedeki en uzun il 14 harf(Afyonkarahisar)
+Ad varchar(14) not null,
+)
+
+ --Bu tabloda illere ait ilçeler tutulur.
+ create table tblIlce(
+ ilceID int identity(1,1) primary key, --identity 1 den baþlayýp 1 arttýrarak ilçelere ID verir.Sadece int deðiþkeninde kullanýlýr.
+ --Türkiyedeki en uzun ilçe 16 harf.
+ Ad varchar(16) not null,
+ --bu kodlarý anlamadým.þimdilik yazýyorum.
+ --ilID char(2) foreign key references tblIl(ilID)
+ -- on delete cascade on update cascade notnull
+)
